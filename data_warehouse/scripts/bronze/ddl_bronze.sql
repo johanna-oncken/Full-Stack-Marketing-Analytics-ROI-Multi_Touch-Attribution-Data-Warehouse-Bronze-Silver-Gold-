03c28b29941efb8b5ba85b258b0576fc2d661d88
johanna-oncken <joh.oncken@gmail.com>
2025-11-28 14:32:23 +0100

    Update ddl_bronze.sql
    This script creates tables in the 'bronze' schema, dropping existing tables if they already exist. Run this script to re-define the DDL structure of 'bronze' tables.

--- data_warehouse/scripts/bronze/ddl_bronze.sql
+++ data_warehouse/scripts/bronze/ddl_bronze.sql
@@ -2,7 +2,7 @@
 ===============================================================================
 DDL Script: Create Bronze Tables
 ===============================================================================
-Script Purpose:
+Script Purpose: 
     This script creates tables in the 'bronze' schema, dropping existing tables 
     if they already exist.
 	Run this script to re-define the DDL structure of 'bronze' Tables
